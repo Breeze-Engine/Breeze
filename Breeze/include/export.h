d200e042d5e84de0576c98676f331b4ffa4c8eb8
krishpranav <krisna.pranav@gmail.com>
2022-05-19 19:15:09 +0530

    Breeze: export + platform

--- Breeze/include/export.h
+++ Breeze/include/export.h
@@ -1,43 +1,43 @@
 #pragma once
 
-#if defined(POMDOG_BUILDING_LIBRARY_EXPORTS) && defined(POMDOG_USING_LIBRARY_EXPORTS)
+#if defined(BREEZE_BUILDING_LIBRARY_EXPORTS) && defined(BREEZE_USING_LIBRARY_EXPORTS)
 #error "exporting error"
 #endif
 
 #if defined(_MSC_VER) && defined(_WIN32)
-#if defined(POMDOG_BUILDING_LIBRARY_EXPORTS)
-#define POMDOG_EXPORT __declspec(dllexport)
-#elif defined(POMDOG_USING_LIBRARY_EXPORTS)
-#define POMDOG_EXPORT __declspec(dllimport)
+#if defined(BREEZE_BUILDING_LIBRARY_EXPORTS)
+#define BREEZE_EXPORT __declspec(dllexport)
+#elif defined(BREEZE_USING_LIBRARY_EXPORTS)
+#define BREEZE_EXPORT __declspec(dllimport)
 #else
-#define POMDOG_EXPORT
+#define BREEZE_EXPORT
 #endif
 
 #elif defined(__CYGWIN__) && defined(__GNUC__)
-#if defined(POMDOG_BUILDING_LIBRARY_EXPORTS)
-#define POMDOG_EXPORT __attribute__((dllexport))
-#elif defined(POMDOG_USING_LIBRARY_EXPORTS)
-#define POMDOG_EXPORT __attribute__((dllimport))
+#if defined(BREEZE_BUILDING_LIBRARY_EXPORTS)
+#define BREEZE_EXPORT __attribute__((dllexport))
+#elif defined(BREEZE_USING_LIBRARY_EXPORTS)
+#define BREEZE_EXPORT __attribute__((dllimport))
 #else
-#define POMDOG_EXPORT
+#define BREEZE_EXPORT
 #endif
 
 #elif defined(__clang__) && defined(__APPLE_CC__)
-#if defined(POMDOG_BUILDING_LIBRARY_EXPORTS)
-#define POMDOG_EXPORT __attribute__((visibility("default")))
-#elif defined(POMDOG_USING_LIBRARY_EXPORTS)
-#define POMDOG_EXPORT
+#if defined(BREEZE_BUILDING_LIBRARY_EXPORTS)
+#define BREEZE_EXPORT __attribute__((visibility("default")))
+#elif defined(BREEZE_USING_LIBRARY_EXPORTS)
+#define BREEZE_EXPORT
 #else
-#define POMDOG_EXPORT
+#define BREEZE_EXPORT
 #endif
 
 #elif defined(__GNUC__) && ((__GNUC__ >= 4) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3))f
-#if defined(POMDOG_BUILDING_LIBRARY_EXPORTS)
-#define POMDOG_EXPORT __attribute__((visibility("default")))
-#elif defined(POMDOG_USING_LIBRARY_EXPORTS)
-#define POMDOG_EXPORT
+#if defined(BREEZE_BUILDING_LIBRARY_EXPORTS)
+#define BREEZE_EXPORT __attribute__((visibility("default")))
+#elif defined(BREEZE_USING_LIBRARY_EXPORTS)
+#define BREEZE_EXPORT
 #else
-#define POMDOG_EXPORT
+#define BREEZE_EXPORT
 #endif
 
 #endif
--- Breeze/include/platform.h
+++ Breeze/include/platform.h
@@ -5,37 +5,37 @@
 #endif
 
 #if defined(linux) || defined(__linux) || defined(__linux__)
-#define POMDOG_PLATFORM_LINUX
+#define BREEZE_PLATFORM_LINUX
 #elif defined(__ORBIS__)
-#define POMDOG_PLATFORM_PLAYSTATION4
+#define BREEZE_PLATFORM_PLAYSTATION4
 #elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
-#define POMDOG_PLATFORM_BSD
+#define BREEZE_PLATFORM_BSD
 #elif defined(_XBOX_ONE)
-#define POMDOG_PLATFORM_XBOX_ONE
+#define BREEZE_PLATFORM_XBOX_ONE
 #elif defined(__NX__)
-#define POMDOG_PLATFORM_SWITCH
+#define BREEZE_PLATFORM_SWITCH
 #elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
-#define POMDOG_PLATFORM_WIN32
+#define BREEZE_PLATFORM_WIN32
 #elif defined(__EMSCRIPTEN__)
-#define POMDOG_PLATFORM_EMSCRIPTEN
+#define BREEZE_PLATFORM_EMSCRIPTEN
 #elif defined(ANDROID) || defined(__ANDROID__)
-#define POMDOG_PLATFORM_ANDROID
+#define BREEZE_PLATFORM_ANDROID
 #elif defined(__APPLE_CC__) && defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE
-#define POMDOG_PLATFORM_APPLE_IOS
+#define BREEZE_PLATFORM_APPLE_IOS
 #elif defined(__APPLE_CC__) && defined(TARGET_OS_MAC) && TARGET_OS_MAC
-#define POMDOG_PLATFORM_MACOSX
+#define BREEZE_PLATFORM_MACOSX
 #elif defined(__QNXNTO__)
-#define POMDOG_PLATFORM_QNXNTO
+#define BREEZE_PLATFORM_QNXNTO
 #elif defined(__native_client__)
-#define POMDOG_PLATFORM_NACL
+#define BREEZE_PLATFORM_NACL
 #elif defined(__CYGWIN__)
-#define POMDOG_PLATFORM_CYGWIN
+#define BREEZE_PLATFORM_CYGWIN
 #elif defined(sun) || defined(__sun)
-#define POMDOG_PLATFORM_SOLARIS
+#define BREEZE_PLATFORM_SOLARIS
 #elif defined(__hpux)
-#define POMDOG_PLATFORM_HPUX
+#define BREEZE_PLATFORM_HPUX
 #elif defined(__ghs__)
-#define POMDOG_PLATFORM_WIIU
+#define BREEZE_PLATFORM_WIIU
 #else
 #error "Sorry, this platform is not supported."
 #endif
