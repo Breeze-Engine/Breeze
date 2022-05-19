c8a211b1f49a70b71efc1206df0ecadac408d1d2
krishpranav <krisna.pranav@gmail.com>
2022-05-19 18:06:57 +0530

    Breeze: unreachable

--- Breeze/include/unreachable.h
+++ Breeze/include/unreachable.h
@@ -2,7 +2,14 @@
 
 #if defined(__clang__)
 #if __has_builtin(__builtin_unreachable)
-#define BREEZE_UNREACHABLE(message) __builtin_unreachable
+#define BREEZE_UNREACHABLE(message) __builtin_unreachable()
 #else
-// 
-#endif 
+#define BREEZE_UNREACHABLE(message)
+#endif
+#elif defined(__GNUC__)
+#define BREEZE_UNREACHABLE(message) __builtin_unreachable()
+#elif defined(_MSC_VER)
+#define BREEZE_UNREACHABLE(message) __assume(0)
+#else
+#define BREEZE_UNREACHABLE(message)
+#endif
