047fd6a6efda45d13832c9d1b73d9377352207c3
krishpranav <krisna.pranav@gmail.com>
2022-05-20 18:46:55 +0530

    Breeze: chrono duration

---
+++ Breeze/chrono/duration.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "breeze/include/conditional_compilation.h"
+
+BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
+#include <chrono>
+BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
+
+namespace breeze {
+    /**
+     * @breif duration
+     */
+    using Duration = std::chrono::duration<double>;
+}
