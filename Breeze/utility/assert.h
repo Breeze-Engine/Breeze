ca87a4b52c78f94d138d6b1c49428ceca754a4d9
krishpranav <krisna.pranav@gmail.com>
2022-05-20 19:07:17 +0530

    Breeze: utility assert

--- Breeze/utility/assert.h
+++ Breeze/utility/assert.h
@@ -7,9 +7,13 @@ BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
 BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
 
 namespace breeze::detail {
-#if defined(NDEBUG)
-#define BREEZE_ASSERT(expression)
-#define BREEEZE_ASSERT_MESSAGE(expression, message)
-#else
-#endif
-}
+
+    #if defined(NDEBUG)
+    #define BREEZE_ASSERT(expression)
+    #define BREEZE_ASSERT_MESSAGE(expression, message)
+    #else
+    #define BREEZE_ASSERT(expression) assert(expression)
+    #define BREEZE_ASSERT_MESSAGE(expression, message) assert(expression)
+    #endif
+
+} 
