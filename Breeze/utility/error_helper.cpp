786baa1362d30dc7fffda21a45f2ac4e66dbf14e
krishpranav <krisna.pranav@gmail.com>
2022-05-24 17:41:03 +0530

    Breeze: utility error helper

--- Breeze/utility/error_helper.cpp
+++ Breeze/utility/error_helper.cpp
@@ -1,13 +1,20 @@
-//
-// Created by Pranav on 24/05/22.
-//
-
 #include "Breeze/utility/error_helper.h"
 
 namespace breeze::detail {
-    std::errc ToErrc(int err) noexcept {
-        static_assert(std::errc::bad_address == static_cast<std::errc>(EFAULT));
 
-        return static_cast<std::errc>(err);
-    }
+std::errc ToErrc(int err) noexcept {
+    static_assert(std::errc::bad_address == static_cast<std::errc>(EFAULT));
+    static_assert(std::errc::bad_file_descriptor == static_cast<std::errc>(EBADF));
+    static_assert(std::errc::invalid_argument == static_cast<std::errc>(EINVAL));
+    static_assert(std::errc::not_a_socket == static_cast<std::errc>(ENOTSOCK));
+    static_assert(std::errc::no_protocol_option == static_cast<std::errc>(ENOPROTOOPT));
+    static_assert(std::errc::operation_in_progress == static_cast<std::errc>(EINPROGRESS));
+    static_assert(std::errc::resource_unavailable_try_again == static_cast<std::errc>(EAGAIN));
+    static_assert(std::errc::operation_would_block == static_cast<std::errc>(EWOULDBLOCK));
+    static_assert(std::errc::interrupted == static_cast<std::errc>(EINTR));
+    static_assert(std::errc::timed_out == static_cast<std::errc>(ETIMEDOUT));
+
+    return static_cast<std::errc>(err);
 }
+
+} 
--- Breeze/utility/error_helper.h
+++ Breeze/utility/error_helper.h
@@ -2,9 +2,9 @@
 
 #include "Breeze/include/conditional_compilation.h"
 
-POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
+BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
 #include <system_error>
-POMDOG_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
+BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
 
 namespace breeze::detail {
     [[nodiscard]] std::ercc ToErrc(int err) noexcept;
