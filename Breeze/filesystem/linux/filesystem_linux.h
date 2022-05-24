6436e83dd317c6509e2bffc436e7b5b3f0d5f858
krishpranav <krisna.pranav@gmail.com>
2022-05-24 17:29:59 +0530

    Breeze: filesystem linux filesystem

--- Breeze/filesystem/linux/filesystem_linux.h
+++ Breeze/filesystem/linux/filesystem_linux.h
@@ -1,8 +1,40 @@
-// filesystem linux
-
 #pragma once
 
 #include "Breeze/utility/errors.h"
 #include <memory>
 #include <string>
 #include <tuple>
+
+namespace breeze::detail::linux {
+
+    [[nodiscard]] std::unique_ptr<Error>
+    CreateNewDirectory(const std::string& path) noexcept;
+
+    [[nodiscard]] std::unique_ptr<Error>
+    CreateDirectories(const std::string& path) noexcept;
+
+    [[nodiscard]] bool
+    Exists(const std::string& path) noexcept;
+
+    [[nodiscard]] bool
+    IsDirectory(const std::string& path) noexcept;
+
+    [[nodiscard]] std::tuple<std::size_t, std::unique_ptr<Error>>
+    GetFileSize(const std::string& path) noexcept;
+
+    [[nodiscard]] std::tuple<std::string, std::unique_ptr<Error>>
+    GetLocalAppDataDirectoryPath() noexcept;
+
+    [[nodiscard]] std::tuple<std::string, std::unique_ptr<Error>>
+    GetAppDataDirectoryPath() noexcept;
+
+    [[nodiscard]] std::tuple<std::string, std::unique_ptr<Error>>
+    GetResourceDirectoryPath() noexcept;
+
+    [[nodiscard]] std::tuple<std::string, std::unique_ptr<Error>>
+    GetTempDirectoryPath() noexcept;
+
+    [[nodiscard]] std::tuple<std::string, std::unique_ptr<Error>>
+    GetCurrentWorkingDirectory() noexcept;
+
+}
