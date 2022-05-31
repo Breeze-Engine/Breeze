c86985abcf08cde40accc449a43c9045c11e99c1
krishpranav <krisna.pranav@gmail.com>
2022-05-31 12:01:51 +0530

    Breeze: filesystem win32

--- Breeze/filesystem/win32/filesystem_win32.h
+++ Breeze/filesystem/win32/filesystem_win32.h
@@ -1,1 +1,20 @@
 // file system windows
+
+#pragma once
+
+#include "Breeze/include/conditional_compilation.h"
+#include "Breeze/utility/errors.h"
+
+BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
+#include <memory>
+#include <string>
+#include <tuple>
+BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
+
+namespace breeze::detail::win32 {
+    [[nodiscard]] std::unique_ptr<Error>
+    CreateNewDirectory(const std::string& path) noexcept;
+
+    [[nodiscard]] std::unique_ptr<Error>
+    CreateDirectories(const std::string& path) noexcept;
+}
