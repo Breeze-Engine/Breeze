4f2fd68ac480af75a84ec15413fd01855a6a26ae
krishpranav <krisna.pranav@gmail.com>
2022-06-27 12:23:32 +0530

    breeze: application game_host[asssets, gamepad, window, keyboard, mouse]

---
+++ Breeze/application/game_host.h
@@ -0,0 +1,19 @@
+#include "Breeze/include/conditional_compilation.h"
+#include "Breeze/include/export.h"
+
+BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
+#include <memory>
+BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
+
+namespace breeze {
+    class AssetManager;
+    class AudioEngine;
+    class GameClock;
+    class Gamepad;
+    class GameWidnow;
+    class HTTPClient;
+    class IOService;
+    class Keyboard;
+    class Mouse;
+    class PCI;
+}
