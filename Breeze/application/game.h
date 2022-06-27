f0ef62122da68410a163502b7fc128e4ddd2b346
krishpranav <krisna.pranav@gmail.com>
2022-06-27 12:07:14 +0530

    Breeze: application game

---
+++ Breeze/application/game.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Breeze/include/conditional_compilation.h"
+#include "Breeze/include/export.h"
+
+BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_BEGIN
+#include <memory>
+BREEZE_SUPPRESS_WARNINGS_GENERATED_BY_STD_HEADERS_END
+
+namespace breeze {
+    class Error;
+}
+
+namespace breeze {
+
+    class BREEZE_EXPORT Game {
+    public:
+        Game();
+        Game(const Game&) = delete;
+
+        Game& operator=(const Game&) = delete;
+
+        virtual ~Game();
+    };  
+}
